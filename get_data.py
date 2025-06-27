6cd9f98a3188f9357aea019dee557842275b1d7d
Mark Baggett <markpbaggett@gmail.com>
2025-06-27 09:39:37 -0500

    If missing things found, report.

--- get_data.py
+++ get_data.py
@@ -3,9 +3,9 @@ from csv import DictReader, DictWriter
 
 # Batch Information
 path_to_texas_aggie_files = "/Volumes/digital_project_management/Texas Aggie"
-batch_folder = "Batch 4 Scans"
-final_csv_name = "texas_aggie_batch_data_new_4.csv"
-batch_title = "texas_aggie_batch_4"
+batch_folder = "Batch 3 Scans"
+final_csv_name = "texas_aggie_batch_data_new_3.csv"
+batch_title = "texas_aggie_batch_3"
 
 
 all_directories = []
@@ -17,8 +17,10 @@ new_data = []
 with open("texas_aggie_batch_data.csv", 'r') as og_data:
     reader = DictReader(og_data)
     for row in reader:
-        current_id = f"texasaggie_{row.get('Date published')}"
-        print(current_id)
+        date = row.get('Date published')
+        if len(date.split('-')) < 3:
+            date = f"{date}-01"
+        current_id = f"texasaggie_{date}"
         if current_id in all_directories:
             volume = row.get('Enumeration').split(':')[0].split('.')[-1]
             issue = row.get('Enumeration').split(':')[1].split('.')[-1]
@@ -26,9 +28,6 @@ with open("texas_aggie_batch_data.csv", 'r') as og_data:
             pages = sum(
                 os.path.isfile(os.path.join(dir_path, f)) for f in os.listdir(dir_path)
             )
-            date = row.get('Date published')
-            if len(date.split('-')) < 3:
-                date = f"{date}-01"
             item = {
                 "item": f"texasaggie_{date}",
                 "title": f"Texas Aggie. Vol. {volume}, No.{issue}",
@@ -41,10 +40,15 @@ with open("texas_aggie_batch_data.csv", 'r') as og_data:
                 "ONI-ChronAm_Batch": batch_title
             }
             new_data.append(item)
-        else:
-            print('no')
+            all_directories.remove(current_id)
+
 
 with open(final_csv_name, 'w') as new_sheet:
     writer = DictWriter(new_sheet, fieldnames=new_data[0].keys())
     writer.writeheader()
     writer.writerows(new_data)
+
+if len(all_directories) > 0 :
+    print("These directories were missing and not added to the batch:")
+    for item in all_directories:
+        print(f"* {item}\n")
